6ae41120e9b1eef9ee6d35c30548d13524157f6b
danbuis <dan.buis88@gmail.com>
2020-08-14 08:56:54 -0400

    Create base class to be filled in

---
+++ src/Geometry2d/BBDPolygon.java
@@ -0,0 +1,33 @@
+package Geometry2d;
+
+public class BBDPolygon implements BBDGeometry{
+    @Override
+    public void translate(double dx, double dy) {
+
+    }
+
+    @Override
+    public void scale(double scaleFactor) {
+
+    }
+
+    @Override
+    public void scaleFromPoint(BBDPoint centerOfScale, double scaleFactor) {
+
+    }
+
+    @Override
+    public void rotate(double degrees) {
+
+    }
+
+    @Override
+    public void rotateAroundPoint(BBDPoint centerOfRotation, double degrees) {
+
+    }
+
+    @Override
+    public BBDPoint center() {
+        return null;
+    }
+}
