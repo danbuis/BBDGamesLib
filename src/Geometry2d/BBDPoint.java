6d2f33e218ecaa456897e316468e0660fbc2f88b
danbuis <dan.buis88@gmail.com>
2020-08-16 13:42:42 -0400

    Correct rotate around point
    Went ahead and cleaned up the documentation too

--- src/Geometry2d/BBDPoint.java
+++ src/Geometry2d/BBDPoint.java
@@ -24,24 +24,32 @@ public class BBDPoint implements BBDGeometry{
         return this.yLoc;
     }
 
+    /**
+     * Translate the point a specified amount in both cardinal directions
+     * @param dx distance to translate on the x-axis
+     * @param dy distance to translate on the y-axis
+     */
     @Override
-    /** Translate the point a specified amount in both cardinal directions */
     public void translate(double dx, double dy) {
         this.xLoc += dx;
         this.yLoc += dy;
     }
 
+    /**
+     * Do nothing because scaling a single point means nothing
+     * @param scaleFactor factor to scale geometry
+     */
     @Override
-    /** Do nothing because scaling a single point means nothing */
-    public void scale(double scaleFactor) {
+        public void scale(double scaleFactor) {
         System.out.println("***WARNING*** Scaling a point without defining another point to scale from is meaningless");
-        return;
     }
 
-    @Override
     /**
      * Use a reference point to scale the geometry.
+     * @param centerOfScale center point to scale from
+     * @param scaleFactor factor to scale geometry
      */
+    @Override
     public void scaleFromPoint(BBDPoint centerOfScale, double scaleFactor) {
         double dx = this.xLoc - centerOfScale.xLoc;
         double dy = this.yLoc - centerOfScale.yLoc;
@@ -49,36 +57,50 @@ public class BBDPoint implements BBDGeometry{
         translate(scaleFactor*dx, scaleFactor*dy);
     }
 
+    /**
+     * Do nothing because rotating a single point means nothing
+     * @param degrees how far to rotate
+     */
     @Override
-    /**Do nothing because rotating a single point means nothing */
     public void rotate(double degrees) {
         System.out.println("***WARNING*** Rotating a point without defining another point to scale from is meaningless");
-        return;
     }
 
-    @Override
     /**
-     * Rotations are +clockwise, and -CCW
+     * Rotations +for counterclockwise, and - for clockwise.  0 degrees is north
+     * on the screen, west is 90 degrees, south is 180 degrees, and east is 270 degree
+     * @param centerOfRotation point to rotate around
+     * @param degrees how far to rotate
      */
+    @Override
     public void rotateAroundPoint(BBDPoint centerOfRotation, double degrees) {
         double deltaX = centerOfRotation.xLoc - this.xLoc;
         double deltaY = centerOfRotation.yLoc - this.yLoc;
 
         //get some polar coordinates
-        double angleFromCenterToOrig = Math.atan2(deltaY, deltaX);
+        double angleFromCenterToOrig = -Math.atan2(deltaY, deltaX);
         double distanceToCenter = distanceToPoint(centerOfRotation);
 
         double angleFromCenterToNew = angleFromCenterToOrig + (Math.PI/180)*degrees;
 
-        translate(Math.cos(angleFromCenterToNew)*distanceToCenter, Math.sin(angleFromCenterToNew)*distanceToCenter);
+        this.xLoc = centerOfRotation.getXLoc() + Math.cos(angleFromCenterToNew)*distanceToCenter;
+        this.yLoc = centerOfRotation.getYLoc() + Math.sin(angleFromCenterToNew)*distanceToCenter;
     }
 
+    /**
+     * Center point.  Should return the point itself
+     * @return this point
+     */
     @Override
-    /**This point is its own center*/
     public BBDPoint center() {
         return this;
     }
 
+    /**
+     * Calculate the distance to another point
+     * @param other the other point
+     * @return how far apart this point and the other point are
+     */
     public double distanceToPoint(BBDPoint other){
         double deltaX = this.xLoc - other.xLoc;
         double deltaY = this.yLoc - other.yLoc;
