c9d0f6390a7c9229642006ba8b1414a8e1ebc07e
danbuis <dan.buis88@gmail.com>
2020-09-21 16:25:16 -0400

    Add delta to area tests to account for floating point drift

--- tests/TestsGeometry2d/TestBBDPolygon.java
+++ tests/TestsGeometry2d/TestBBDPolygon.java
@@ -180,10 +180,10 @@ public class TestBBDPolygon {
     public void testArea(){
         BBDPolygon square = this.buildSquare();
         //check basic method
-        assertEquals(4, square.area());
+        assertEquals(4, square.area(), 0.00001);
         //check if object is modified
         square.scale(2);
-        assertEquals(16, square.area());
+        assertEquals(16, square.area(), 0.00001);
 
         //construct a square that has co-linear points on the edges.
         BBDPoint point1 = new BBDPoint(1,1);
@@ -196,11 +196,11 @@ public class TestBBDPolygon {
         BBDPoint point45 = new BBDPoint(0,1);
 
         BBDPoint[] points = {point1, point15, point2, point25, point3, point35, point4, point45};
-        assertEquals(4, new BBDPolygon(points).area());
+        assertEquals(4, new BBDPolygon(points).area(), 0.00001);
 
-        //make a concave polygon, and make sure that he first 3 points make a triangle NOT inside
+        //make a concave polygon, and make sure that the first 3 points make a triangle NOT inside
         point2 = new BBDPoint(0,0);
         BBDPoint[] points2 = {point15, point2, point25, point3, point35, point4, point45, point1};
-        assertEquals(3, new BBDPolygon(points2).area());
+        assertEquals(3, new BBDPolygon(points2).area(), 0.00001);
     }
 }
