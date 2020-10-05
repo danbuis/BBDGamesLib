b851925418b31ce998459b298dd568354295829d
danbuis <dan.buis88@gmail.com>
2020-10-05 11:24:58 -0400

    Make GameItem2d superficially compatible with BBDPolygon
    A Polygon can be attached to the class on construction, and a flag can be set as to whetehr or not the Polygon needs to be updated during use.  Maybe the user just wants to programmatically create a shape and use that to render.  If so then they polygon will not need to be updated with translates etc.  if however the polygon needs to do things like measure dustance, area etc, then it needs to be accurate to the displayed form.

--- src/GameEngine/GameItem2d.java
+++ src/GameEngine/GameItem2d.java
@@ -1,5 +1,7 @@
 package GameEngine;
 
+import Geometry2d.BBDPoint;
+import Geometry2d.BBDPolygon;
 import openGL.Mesh;
 import openGL.ShaderProgram;
 import org.joml.Matrix4f;
@@ -17,12 +19,18 @@ public class GameItem2d extends GameItem{
     public final ShaderProgram shader;
     private int layer;
     private final float LAYER_INTERVAL = (float)0.001;
+    private BBDPolygon shape;
+    private boolean shapeInteracts;
 
-    public GameItem2d(Mesh mesh, ShaderProgram shaderProgram, int layer) {
+    public GameItem2d(Mesh mesh, ShaderProgram shaderProgram, BBDPolygon shape,  int layer, boolean shapeInteracts) {
         super(mesh, shaderProgram);
         this.mesh = mesh;
         this.shader = shaderProgram;
-        position = new Vector3f();
+        this.layer = layer;
+        this.shape = shape;
+        this.shapeInteracts = shapeInteracts;
+        BBDPoint center = shape.center();
+        position = new Vector3f((float)center.getXLoc(), (float)center.getYLoc(), layer*LAYER_INTERVAL);
         scale = 1;
         rotation = new Vector3f();
     }
@@ -34,7 +42,20 @@ public class GameItem2d extends GameItem{
     public void setPosition(float x, float y) {
         this.position.x = x;
         this.position.y = y;
-        this.position.z = this.layer * LAYER_INTERVAL;
+
+        if(shapeInteracts){
+            BBDPoint currentCenter = shape.center();
+            this.translate((float)currentCenter.getXLoc() - x, (float)currentCenter.getYLoc() - y);
+        }
+    }
+
+    public void translate(float x, float y) {
+        this.position.x += x;
+        this.position.y += y;
+
+        if(shapeInteracts){
+            shape.translate(x,y);
+        }
     }
 
     public float getScale() {
@@ -42,15 +63,62 @@ public class GameItem2d extends GameItem{
     }
 
     public void setScale(float scale) {
+        if(shapeInteracts){
+            this.shape.scale(scale/this.scale);
+        }
+
         this.scale = scale;
+    }
+
+    public void scale(float scaleFactor){
+        if(shapeInteracts){
+            this.shape.scale(scaleFactor);
+        }
+
+        this.scale = scale * scaleFactor;
+    }
+
+    public void scaleFromPoint(BBDPoint point, float scaleFactor){
+        if(shapeInteracts){
+            this.shape.scaleFromPoint(point, scaleFactor);
+        }
+        // translate
+        float deltaX = this.position.x - (float)point.getXLoc();
+        float deltaY = this.position.y - (float)point.getYLoc();
+        this.position.x = this.position.x + deltaX * scaleFactor;
+        this.position.y = this.position.y + deltaY * scaleFactor;
+        // scale
+        this.scale = scaleFactor;
     }
 
     public Vector3f getRotation() {
         return rotation;
     }
+    //vector3f.rotateAxis is like our rotate around point
 
     public void setRotation(float z) {
+        if (shapeInteracts){
+            float currentRotation = this.rotation.z;
+            this.shape.rotate(currentRotation - z);
+        }
+
         this.rotation.z = z;
+    }
+
+    public void rotate(float angle){
+        this.rotation.rotateZ(angle);
+
+        if (shapeInteracts){
+            this.shape.rotate(angle);
+        }
+    }
+
+    public void rotateAroundPoint(BBDPoint point, float angle){
+        this.rotation.rotateAxis(angle, (float)point.getXLoc(), (float)point.getYLoc(), 0);
+
+        if(shapeInteracts){
+            this.shape.rotateAroundPoint(point, angle);
+        }
     }
 
     public Mesh getMesh() {
