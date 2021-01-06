d6dc6d6561ddccf873336c4d392f815bfef0930e
danbuis <dan.buis88@gmail.com>
2021-01-06 13:00:24 -0500

    Add a new die class

---
+++ src/BBDGameLibrary/GameEngine/Die.java
@@ -0,0 +1,38 @@
+package BBDGameLibrary.GameEngine;
+
+import java.util.Arrays;
+
+public class Die {
+
+    //How many sides does this die have
+    public final int sides;
+
+    //if it has non-numeric faces what are they?
+    public final Object[] faces;
+
+    public Object currentFace;
+
+    public Die(int sides){
+        this.sides = sides;
+
+        Integer[] faces = new Integer[sides];
+        for (int i = 1; i <= sides; i++){
+            faces[i-1] = i;
+        }
+        this.faces = faces;
+    }
+
+    public Die(Object[] faces){
+        this.sides = faces.length;
+        this.faces = faces;
+    }
+
+    public void roll(){
+        this.currentFace = this.faces[(int)Math.floor(Math.random() * this.sides)];
+    }
+
+    public void setToFace(Object facing){
+        int indexOfFace = Arrays.asList(this.faces).indexOf(facing);
+        this.currentFace = facing;
+    }
+}
