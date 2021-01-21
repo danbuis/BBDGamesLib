c6b90b62be86b3e6dc5b9982dcbf7fd6e40f7b72
danbuis <dan.buis88@gmail.com>
2021-01-21 13:38:12 -0500

    Change test params
    0,0 isn't the center, its the lower left, which throws things off.

--- tests/TestsGameEngine/TestSelection.java
+++ tests/TestsGameEngine/TestSelection.java
@@ -21,7 +21,7 @@ public class TestSelection {
     Window window = null;
     boolean windowInit = false;
     public void initWindow(){
-        window = new Window("test", 5, 5, true, new Window.WindowOptions());
+        window = new Window("test", 200, 200, true, new Window.WindowOptions());
         window.init();
     }
 
@@ -52,21 +52,98 @@ public class TestSelection {
         MouseSelectionDetector detector = new MouseSelectionDetector();
         GameItem selectedItem = null;
 
-        selectedItem = detector.selectItem(itemList, window, new Vector2d(0,0), camera, 0.00001f);
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0.00001f);
 
         assertEquals(item, selectedItem);
 
         item.setPosition(400,400,0);
-        selectedItem = detector.selectItem(itemList, window, new Vector2d(0,0), camera, 0.00001f);
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0.00001f);
         assertNull(selectedItem);
+
+        item.setPosition(0,0,0);
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0.00001f);
+        assertEquals(item, selectedItem);
     }
 
     //test a rotated item where we check the old corner, but now there isn't anything there.
+    @Test
+    public void testRotatedItem(){
+        if(!windowInit){
+            initWindow();
+        }
+        BBDPolygon poly = this.buildSquare();
+        Mesh mesh = Mesh.buildMeshFromPolygon(poly, null);
+
+        GameItem item = new GameItem(mesh, null);
+        ArrayList<GameItem> itemList= new ArrayList<>();
+        itemList.add(item);
+        Camera camera = new Camera();
+        camera.setPosition(0,0,1);
+        MouseSelectionDetector detector = new MouseSelectionDetector();
+        GameItem selectedItem = null;
+
+        item.setPosition(0.95f, 0.95f, 0);
+        System.out.println(item.getMeshVerticesRealLocations()[0]);
+        System.out.println(item.getMeshVerticesRealLocations()[1]);
+        System.out.println(item.getMeshVerticesRealLocations()[2]);
+        System.out.println(item.getMeshVerticesRealLocations()[3]);
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0);
+        assertEquals(item, selectedItem);
+
+        item.setRotation(0,0, (float)Math.PI/4);
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0.00001f);
+        assertNull(selectedItem);
+    }
 
     //test nearby item
+    @Test
+    public void nearbyTest(){
+        if(!windowInit){
+            initWindow();
+        }
+        BBDPolygon poly = this.buildSquare();
+        Mesh mesh = Mesh.buildMeshFromPolygon(poly, null);
+
+        GameItem item = new GameItem(mesh, null);
+        GameItem item2 = new GameItem(mesh, null);
+        ArrayList<GameItem> itemList= new ArrayList<>();
+        itemList.add(item);
+        itemList.add(item2);
+        Camera camera = new Camera();
+        camera.setPosition(0,0,1);
+        item2.setPosition(2,0,0);
+        MouseSelectionDetector detector = new MouseSelectionDetector();
+        GameItem selectedItem = null;
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0.00001f);
+        assertEquals(item, selectedItem);
+    }
 
     //test stacked items
+    @Test
+    public void testStacked(){
+        if(!windowInit){
+            initWindow();
+        }
+        BBDPolygon poly = this.buildSquare();
+        Mesh mesh = Mesh.buildMeshFromPolygon(poly, null);
 
-    //test multiple coplaner items.
+        GameItem item = new GameItem(mesh, null);
+        GameItem item2 = new GameItem(mesh, null);
+        ArrayList<GameItem> itemList= new ArrayList<>();
+        itemList.add(item);
+        itemList.add(item2);
+        Camera camera = new Camera();
+        camera.setPosition(0,0,1);
+        item2.setPosition(0,0,-1);
+
+        MouseSelectionDetector detector = new MouseSelectionDetector();
+        GameItem selectedItem = null;
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0.00001f);
+        assertEquals(item, selectedItem);
+
+        item.setPosition(400,400,0);
+        selectedItem = detector.selectItem(itemList, window, new Vector2d(100,100), camera, 0.00001f);
+        assertEquals(item2, selectedItem);
+    }
 
 }
